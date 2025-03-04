6ef5deda704508b199c51b0dd07f004589aab46f
Artem Marukhin <Artemmarukhin@yandex.ru>
2025-03-05 00:30:57 +0200

    Pawn is complete

--- src/Chess/Pawn.java
+++ src/Chess/Pawn.java
@@ -11,49 +11,66 @@ public class Pawn extends ChessPiece{
     }
 
     @Override
-    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-//            if (chessBoard.checkPos(line) && chessBoard.checkPos(column) && chessBoard.checkPos(toLine) && chessBoard.checkPos(toColumn)) {
-//                if (chessBoard.board[line][column] != chessBoard.board[toLine][toColumn]) {
-//                    if (color.equals("White") && line == 1) {
-//                        if (chessBoard.board[line + 2][column] == chessBoard.board[toLine][toColumn] || chessBoard.board[line + 1][column] == chessBoard.board[toLine][toColumn]){
-//                            return true;
-//                        }
-//                    } else if(color.equals("White") && line != 1){
-//                        if (chessBoard.board[line + 1][column] == chessBoard.board[toLine][toColumn]){
-//                            return true;
-//                        }
-//                    }
-//                    if (color.equals("Black") && line == 6) {
-//                        if(chessBoard.board[line - 2][column] == chessBoard.board[toLine][toColumn] || chessBoard.board[line - 1][column] == chessBoard.board[toLine][toColumn]){
-//                            return true;
-//                        } else if (color.equals("Black") && line != 6){
-//                            if (chessBoard.board[line - 1][column] == chessBoard.board[toLine][toColumn]) {
-//                                return true;
-//                            }
-//                        }
-//                    } return false;
-//                }return false;
+//    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+//        if (chessBoard.checkPos(line) && chessBoard.checkPos(column) && chessBoard.checkPos(toLine) && chessBoard.checkPos(toColumn)){
+//            if (chessBoard.board[line][column].getColor().equals("White")){
+//                if (chessBoard.board[line][column] == chessBoard.board[toLine][toColumn] && chessBoard.board[toLine][toColumn] == chessBoard.board[line+1][column]){
+//                    return chessBoard.board[toLine][toColumn] == null;
+//                }
+//                if (line + 2 == toLine && chessBoard.board[toLine][column] == null && line == 1 && chessBoard.board[line][column] == chessBoard.board[1][column]){
+//                    return chessBoard.board[toLine+1][toColumn] == null;
+//                }
+//            } else if (chessBoard.board[line][column].getColor().equals("Black")) {
+//                if (chessBoard.board[line][column] == chessBoard.board[toLine][toColumn] && chessBoard.board[toLine][toColumn] == chessBoard.board[line-1][column]) {
+//                    return chessBoard.board[toLine][toColumn] == null;
+//                }
+//                if (line - 2 == toLine && chessBoard.board[toLine][column] == null && chessBoard.board[line][column] == chessBoard.board[6][column]) {
+//                    return chessBoard.board[toLine - 1][toColumn] == null;
+//                }
 //            }return false;
+////            } else if (chessBoard.board[line][column] == chessBoard.board[line+1][toColumn] && this.color.equals("White")){
+////                return chessBoard.board[toLine][toColumn] == null;
+////            } else if (chessBoard.board[line][column] == chessBoard.board[line-1][toColumn] && this.color.equals("Black")) {
+////                return chessBoard.board[toLine][toColumn] == null;
+////            } return false;
+//        }return false;
+//    }
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (chessBoard.checkPos(line) && chessBoard.checkPos(column) && chessBoard.checkPos(toLine) && chessBoard.checkPos(toColumn) && chessBoard.board[line][column] != null) { // check that position in board
+            if (column == toColumn) { // check that we don't want to eat
+                int direction;
+                int start;
 
+                if (color.equals("White")) {  // for white piece
+                    direction = 1;
+                    start = 1;
+                } else { // for black piece
+                    direction = -1;
+                    start = 6;
+                }
 
-        if (chessBoard.checkPos(line) && chessBoard.checkPos(column) && chessBoard.checkPos(toLine) && chessBoard.checkPos(toColumn)
-                && chessBoard.board[line][column] != chessBoard.board[toLine][toColumn]){
-            int dir;
-            int startPos;
-            if (color.equals("White")){
-                dir = 1;
-                startPos = 1;
-            } else if (color.equals("Black")) {
-                dir = -1;
-                startPos = 6;
+                if (line + direction == toLine) {  //check direction
+                    return chessBoard.board[toLine][toColumn] == null;
+                }
+
+                if (line == start && line + 2 * direction == toLine) {
+                    return chessBoard.board[toLine][toColumn] == null && chessBoard.board[line + direction][column] == null; // check that positions is null
+                }
+
+            } else { // want to eat piece
+
+                if ((column - toColumn == 1 || column - toColumn == -1) && (line - toLine == 1 || line - toLine == -1) && // check that piece another color
+                        chessBoard.board[toLine][toColumn] != null) {
+                    return !chessBoard.board[toLine][toColumn].getColor().equals(color);
+                } else return false;
             }
-
-
-        }return false;
+        }
+        return false;
     }
 
     @Override
     public String getSymbol() {
         return "P";
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        int a =2;
+        int b = 2;
+        if(1+1 == a){
+            return b == a;
+        }
         System.out.println("Hello world!");
     }
 }
