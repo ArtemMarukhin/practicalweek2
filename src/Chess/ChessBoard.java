7aa532c1f84b84a24631c36186b5e5744ce98416
ArtemMarukhin <Artemmarukhin@yandex.ru>
2025-03-11 18:58:58 +0200

    Chess complete

--- src/Chess/ChessBoard.java
+++ src/Chess/ChessBoard.java
@@ -52,4 +52,75 @@ public class ChessBoard {
     public boolean checkPos(int pos) {
         return pos >= 0 && pos <= 7;
     }
+    public boolean castling0() {
+        if (nowPlayer.equals("White")) {
+            if (board[0][0] == null || board[0][4] == null) return false;
+            if (board[0][0].getSymbol().equals("R") && board[0][4].getSymbol().equals("K") && board[0][1] == null && board[0][2] == null && board[0][3] == null) {
+                if (board[0][0].getColor().equals("White") && board[0][4].getColor().equals("White") &&
+                        board[0][0].check && board[0][4].check &&
+                        !new King("White").isUnderAttack(this, 0, 2)) {
+                    board[0][4] = null;
+                    board[0][1] = new King("White");
+                    board[0][1].check = false;
+                    board[0][0] = null;
+                    board[0][2] = new Rook("White");
+                    board[0][2].check = false;
+                    nowPlayer = "Black";
+                    return true;
+                } else return false;
+            } else return false;
+        } else {
+            if (board[7][0] == null || board[7][4] == null) return false;
+            if (board[7][0].getSymbol().equals("R") && board[7][4].getSymbol().equals("K") && board[7][1] == null && board[7][2] == null && board[7][3] == null) {
+                if (board[7][0].getColor().equals("Black") && board[7][4].getColor().equals("Black") &&
+                        board[7][0].check && board[7][4].check &&
+                        !new King("Black").isUnderAttack(this, 7, 2)) {
+                    board[7][4] = null;
+                    board[7][1] = new King("Black");
+                    board[7][1].check = false;
+                    board[7][0] = null;
+                    board[7][2] = new Rook("Black");
+                    board[7][2].check = false;
+                    nowPlayer = "White";
+                    return true;
+                } else return false;
+            } else return false;
+        }
+    }
+
+    public boolean castling7() {
+        if (nowPlayer.equals("White")) {
+            if (board[0][7] == null || board[0][4] == null) return false;
+            if (board[0][7].getSymbol().equals("R") && board[0][4].getSymbol().equals("K") && board[0][6] == null && board[0][5] == null) {
+                if (board[0][7].getColor().equals("White") && board[0][4].getColor().equals("White") &&
+                        board[0][7].check && board[0][4].check &&
+                        !new King("White").isUnderAttack(this, 0, 6)) {
+                    board[0][4] = null;
+                    board[0][6] = new King("White");
+                    board[0][6].check = false;
+                    board[0][7] = null;
+                    board[0][5] = new Rook("White");
+                    board[0][5].check = false;
+                    nowPlayer = "Black";
+                    return true;
+                } else return false;
+            } else return false;
+        } else {
+            if (board[7][7] == null || board[7][4] == null) return false;
+            if (board[7][7].getSymbol().equals("R") && board[7][4].getSymbol().equals("K") && board[7][6] == null && board[7][5] == null) {
+                if (board[7][7].getColor().equals("Black") && board[7][4].getColor().equals("Black") &&
+                        board[7][7].check && board[7][4].check &&
+                        !new King("Black").isUnderAttack(this, 7, 6)) {
+                    board[7][4] = null;
+                    board[7][6] = new King("Black");
+                    board[7][6].check = false;
+                    board[7][7] = null;
+                    board[7][5] = new Rook("Black");
+                    board[7][5].check = false;
+                    nowPlayer = "White";
+                    return true;
+                } else return false;
+            } else return false;
+        }
+    }
 }
